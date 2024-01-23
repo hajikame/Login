4e29fefe1ab2cd608a58d2d761ce6dc29497074d
Hajikame <Abdxkml@gmail.com>
2024-01-23 12:23:58 -0500

    updated

---
+++ Cards/src/Components/Card.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import logo from '../assets/logo.jpg'
+const Card = () => {
+  return (
+    <>
+      <div className="card">
+      <div className='cards'>
+      <img src={logo} alt="" />
+      <pre> Somali Bus</pre>
+      </div>
+
+      <div className='cards'>
+      <img src={logo} alt="" />
+      <pre> Somali Bus</pre>
+      </div>
+      <div className='cards'>
+      <img src={logo} alt="" />
+      <pre> Somali Bus</pre>
+      </div>
+
+      <div className='cards'>
+      <img src={logo} alt="" />
+      <pre> Somali Bus</pre>
+      </div>
+
+      <div className='cards'>
+      <img src={logo} alt="" />
+      <pre> Somali Bus</pre>
+      </div>
+
+      <div className='cards'>
+      <img src={logo} alt="" />
+      <pre> Somali Bus</pre>
+      </div>
+      
+     
+   
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default Card
